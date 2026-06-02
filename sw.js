344b9dde94f7e0354965c84a8babe472faf0740a
juliachensuite <julia@thesuite.com>
2026-06-02 00:30:47 -0400

    v3 for consistency

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "counter-v2";
+const CACHE = "counter-v3";
 const FILES = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
